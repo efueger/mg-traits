9bba55daae02189e598cfe21132e3920b9821596
pereiramemo <pereiramemo@gmail.com>
2016-06-03 18:32:37 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -101,12 +101,7 @@ fi
 
 echo "This job tmp dir: ${THIS_JOB_TMP_DIR}"; 
 
-
-THIS_JOB_TMP_DIR=$(readlink -m "${RUNNING_JOBS_DIR}/job-test")
-THIS_JOB_TMP_DIR_DATA="${THIS_JOB_TMP_DIR}/data/"
-SINA_LOG_DIR="${THIS_JOB_TMP_DIR}/sina_log"
-
-rm -r "${THIS_JOB_TMP_DIR}"
+rm -r "${THIS_JOB_TMP_DIR}" # CHANGE THIS FOR REAL DATA!!!!!!!!!!
 mkdir "${THIS_JOB_TMP_DIR}" && cd "${THIS_JOB_TMP_DIR}"
 mkdir "${THIS_JOB_TMP_DIR_DATA}" && mkdir "${SINA_LOG_DIR}"
 
