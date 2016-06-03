b339e044afc297cfb857370f60382c571d6e9af7
pereiramemo <pereiramemo@gmail.com>
2016-06-03 20:53:56 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -127,7 +127,7 @@ REGEX='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]
 
 if [[ ! ${MG_URL} =~ ${REGEX} ]]; then
   
-  DB_COM=$( db_error_comm hola2 ); 
+  db_error_comm hola2; 
   email_comm "Invalid URL ${MG_URL} output db: ${DB_COM} ${SAMPLE_LABEL}"
   
   exit 1
