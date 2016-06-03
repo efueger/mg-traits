a8d964edebc377c301f7fa8fc72de8692945603f
pereiramemo <pereiramemo@gmail.com>
2016-06-03 18:39:42 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -101,7 +101,7 @@ fi
 
 echo "This job tmp dir: ${THIS_JOB_TMP_DIR}"; 
 
-rm -r "${THIS_JOB_TMP_DIR}" # CHANGE THIS FOR REAL DATA!!!!!!!!!!
+rm -r $(ls -lrt | tail -1 | awk '{ print $8}') # CHANGE THIS FOR REAL DATA!!!!!!!!!!
 mkdir "${THIS_JOB_TMP_DIR}" && cd "${THIS_JOB_TMP_DIR}"
 mkdir "${THIS_JOB_TMP_DIR_DATA}" && mkdir "${SINA_LOG_DIR}"
 
