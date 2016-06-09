8489c2d599847cc03d40558f08ffff847d9872d8
pereiramemo <pereiramemo@gmail.com>
2016-06-09 14:30:46 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -361,7 +361,7 @@ ${DB}/index/silva-arc-16s-db:${DB}/rRNA_databases/silva-euk-18s-id95.fasta,\
 ${DB}/index/silva-euk-18s-db --blast 1 --fastx --aligned sortmerna.rDNA -v --log -m ${MEM} --best 1 > sortmerna.log
 
 
-#${sortmerna_runner} "${RAW_FASTA}" "${NSLOTS}"
+##${sortmerna_runner} "${RAW_FASTA}" "${NSLOTS}"
 
 if [[ "${ERROR_SORTMERNA}" -ne "0" ]]; then
   email_comm "${sortmerna} --reads ${RAW_FASTA} -a ${NSLOTS} --ref ${DB}/rRNA_databases/silva-bac-16s-id90.fasta ...
