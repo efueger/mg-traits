44a035ad5c5a36f27fdc6b015e1d03b03fac61f0
pereiramemo <pereiramemo@gmail.com>
2016-06-10 21:51:03 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -89,7 +89,14 @@ function db_error_comm() {
   WHERE sample_label = '${SAMPLE_LABEL}' AND id = '${MG_ID}';" | psql -U "${target_db_user}" -h "${target_db_host}" -p "${target_db_port}" -d "${target_db_name}"
 }
 
+
+DB_RESULT=$( echo "UPDATE mg_traits.mg_traits_jobs SET time_started = now(), job_id = ${JOB_ID}, cluster_node = '${HOSTNAME}' WHERE sample_label = '${SAMPLE_LABEL}' AND id = ${MG_ID};" \
+| psql -U "${target_db_user}" -h "${target_db_host}" -p "${target_db_port}" -d "${target_db_name}" )
+
+
 cd "${THIS_JOB_TMP_DIR}"
+
+
 
 ###########################################################################################################
 # 4 - Finish Jobs
@@ -132,7 +139,7 @@ echo ARBHOME=$ARBHOME >> 00-environment
 echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH >> 00-environment
 
 
-email_comm "running runner_finish"
+
 qsub -pe threaded 8 -l h=\!mg32 -N $FINISHJOBID -o $THIS_JOB_TMP_DIR -e $THIS_JOB_TMP_DIR -l ga -j y -terse -P megx.p -R y -m sa -M $mt_admin_mail -hold_jid $FGS_JOBARRAYID,$SINA_JOBARRAYID  /bioinf/home/epereira/workspace/mg-traits/resources/finish_runner.sh $THIS_JOB_TMP_DIR
 
 
