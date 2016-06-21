d7dac770b12ef133c5dd3cc8753b739606fca9ff
pereiramemo <pereiramemo@gmail.com>
2016-06-21 15:05:18 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -112,7 +112,7 @@ fi
 
 echo "This job tmp dir: ${THIS_JOB_TMP_DIR}"; 
 
-rm -r ${THIS_JOB_TMP_DIR}  # CHANGE THIS FOR REAL DATA!!!!!!!!!! 
+#rm -r ${THIS_JOB_TMP_DIR}  # CHANGE THIS FOR REAL DATA!!!!!!!!!! 
 qdel -u megxnet  # CHANGE THIS FOR REAL DATA!!!!!!!!!! 
  
 mkdir "${THIS_JOB_TMP_DIR}" && cd "${THIS_JOB_TMP_DIR}"
