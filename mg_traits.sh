7f6ab3d6cfa4f60c3571153ada7c43c4406a6a90
pereiramemo <pereiramemo@gmail.com>
2016-06-22 16:09:48 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -130,59 +130,74 @@ fi
 ###########################################################################################################
 # 3 - Download file from MG URL
 ###########################################################################################################
-
-# validate MG URL 
-echo "${MG_URL}"
-REGEX='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
-  
-if [[ ! ${MG_URL} =~ ${REGEX} ]]; then
-  email_comm "Invalid URL ${MG_URL} output db: ${DB_COM} ${SAMPLE_LABEL}"
-  db_error_comm "Not valid URL: ${MG_URL}";
-  exit 1
-fi 
-
-
-#  CHANGE THIS FOR REAL DATA !!!!!!!!!!!!!!!!!!!
-# check if it already exist on our DB                    
-# URLDB=$(psql -t -U "${target_db_user}" -h "${target_db_host}" -p "${target_db_port}" -d "${target_db_name}" -c \
-# "SELECT count(*) FROM mg_traits.mg_traits_jobs where mg_url = '${MG_URL}' AND sample_label NOT ILIKE '%test% AND return_code = 0'")
-#     
-# if [[ "${URLDB}" -gt 1 ]]; then 
-#   email_comm "The URL "${MG_URL}" has been already succesfully crunched. If the file is different please change the file name"
-#   db_error_comm "The URL ${MG_URL} has been already succesfully crunched. If the file is different please change the file name."
+# 
+# # validate MG URL 
+# echo "${MG_URL}"
+# REGEX='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+#   
+# if [[ ! ${MG_URL} =~ ${REGEX} ]]; then
+#   email_comm "Invalid URL ${MG_URL} output db: ${DB_COM} ${SAMPLE_LABEL}"
+#   db_error_comm "Not valid URL: ${MG_URL}";
+#   exit 1
+# fi 
+# 
+# 
+# #  CHANGE THIS FOR REAL DATA !!!!!!!!!!!!!!!!!!!
+# # check if it already exist on our DB                    
+# # URLDB=$(psql -t -U "${target_db_user}" -h "${target_db_host}" -p "${target_db_port}" -d "${target_db_name}" -c \
+# # "SELECT count(*) FROM mg_traits.mg_traits_jobs where mg_url = '${MG_URL}' AND sample_label NOT ILIKE '%test% AND return_code = 0'")
+# #     
+# # if [[ "${URLDB}" -gt 1 ]]; then 
+# #   email_comm "The URL "${MG_URL}" has been already succesfully crunched. If the file is different please change the file name"
+# #   db_error_comm "The URL ${MG_URL} has been already succesfully crunched. If the file is different please change the file name."
+# #   exit 1
+# # fi
+# 
+# 
+# # download MG_URL
+# printf "Downloading ${MG_URL} to ${RAW_DOWNLOAD}..."
+# curl -s "${MG_URL}" > "${RAW_DOWNLOAD}"
+# 
+# if [[ "$?" -ne "0" ]]; then 
+#   email_comm "Could not retrieve ${MG_URL}"
+#   db_error_comm  "Could not retrieve ${MG_URL}"
 #   exit 1
 # fi
+# 
+# # compress data
+# gunzip -qc "${RAW_DOWNLOAD}" > "${RAW_FASTA}"
+# if [[ "$?" -ne "0" ]]; then 
+#   echo "File was uncompressed"
+#   rm "${RAW_FASTA}"; mv "${RAW_DOWNLOAD}" "${RAW_FASTA}" # NEEDS REVIEW: TRAP AND DB COMMUNICATION?
+# fi
+#  
 
-
-# download MG_URL
-printf "Downloading ${MG_URL} to ${RAW_DOWNLOAD}..."
-curl -s "${MG_URL}" > "${RAW_DOWNLOAD}"
-
-if [[ "$?" -ne "0" ]]; then 
-  email_comm "Could not retrieve ${MG_URL}"
-  db_error_comm  "Could not retrieve ${MG_URL}"
-  exit 1
-fi
-
-# compress data
-gunzip -qc "${RAW_DOWNLOAD}" > "${RAW_FASTA}"
-if [[ "$?" -ne "0" ]]; then 
-  echo "File was uncompressed"
-  rm "${RAW_FASTA}"; mv "${RAW_DOWNLOAD}" "${RAW_FASTA}" # NEEDS REVIEW: TRAP AND DB COMMUNICATION?
-fi
-  
 ###########################################################################################################
-# 4 -  Validate file
+# 4 - Preprocess data
+###########################################################################################################
+
+echo SAMPLE_LABEL=${SAMPLE_LABEL} > 00-environment
+echo MG_ID=${MG_ID} >>  00-environment
+echo target_db_user=${target_db_user} >> 00-environment
+echo target_db_host=${target_db_host} >> 00-environment
+echo target_db_port=${target_db_port} >> 00-environment
+echo target_db_name=${target_db_name} >> 00-environment
+
+"${preprocess}" "${SAMPLE_LABEL}" "${NSLOTS}"
+PROCESS_FASTA="01-process.SR_rmadapt_nodup.fasta"
+
+###########################################################################################################
+# 5 -  Validate file
 ###########################################################################################################
 
 printf "Validating file..."
-${fasta_file_check} "${RAW_FASTA}" "${FASTA_BAD}"
+${fasta_file_check} "${PROCESS_FASTA}" "${FASTA_BAD}"
 FASTA_ERROR_CODE="$?"
 
 if [[ "${FASTA_ERROR_CODE}" -ne "0" ]]; then
   FASTA_BAD_HEADER=$(grep '>' "${FASTA_BAD}" | tr -d '>'); 
   
-  email_comm "${MG_URL} is not a valid FASTA file. FASTA validation failed at sequence ${FASTA_BAD_HEADER}, error: ${FASTA_ERROR_CODE}. See ${FASTA_BAD}. ${fasta_file_check} ${RAW_FASTA} ${FASTA_BAD}"
+  email_comm "${MG_URL} is not a valid FASTA file. FASTA validation failed at sequence ${FASTA_BAD_HEADER}, error: ${FASTA_ERROR_CODE}. See ${FASTA_BAD}. ${fasta_file_check} ${PROCESS_FASTA} ${FASTA_BAD}"
   db_error_comm  "${MG_URL} is not a valid FASTA file. Sequence validation failed. Error: ${FASTA_ERROR_CODE}. See ${FASTA_BAD}"
   
   exit 1
@@ -190,7 +205,7 @@ fi
 
 
 ###########################################################################################################
-# 5 - Check for utilities and directories
+# 6 - Check for utilities and directories
 ###########################################################################################################
 
 ERROR_MESSAGE=$(\
@@ -199,7 +214,6 @@ check_required_readable_directories "${mg_traits_dir}";
 check_required_programs "${cd_hit_dup}" "${cd_hit_est}" "${cd_hit_mms}" "${uproc}" "${r_interpreter}";\
 )
 
-
 if [[ -n "${ERROR_MESSAGE}" ]]; then
   email_comm "Not found ${ERROR_MESSAGE}"
   db_error_comm "Not found ${ERROR_MESSAGE}"
@@ -207,7 +221,7 @@ if [[ -n "${ERROR_MESSAGE}" ]]; then
 fi
 
 ###########################################################################################################
-# 6 - Download data files from SVN
+# 7 - Download data files from SVN
 ###########################################################################################################
 
 # pfam downlaod
@@ -243,15 +257,15 @@ if [[ "$?" -ne "0" ]]; then
 fi
 
 ###########################################################################################################
-# 7 - Check for duplicates
+# 8 - Check for duplicates
 ###########################################################################################################
 
 printf "Removing duplicated sequences..."
-"${cd_hit_dup}" -d 1 -i "${RAW_FASTA}" -o "${UNIQUE}" > "${UNIQUE_LOG}"
+"${cd_hit_dup}" -d 1 -i "${PROCESS_FASTA}" -o "${UNIQUE}" > "${UNIQUE_LOG}"
 CD_HIT_ERROR_CODE="$?"
 
 if [[ "${CD_HIT_ERROR_CODE}" -ne "0" ]]; then 
-  email_comm "${cd_hit_dup} -i ${RAW_FASTA} -o /dev/null > ${UNIQUE_LOG}
+  email_comm "${cd_hit_dup} -i ${PROCESS_FASTA} -o /dev/null > ${UNIQUE_LOG}
 exited with RC ${CD_HIT_ERROR_CODE} in job ${JOB_ID}\nFiles are at: ${FAILED_JOBS_DIR}/job-${JOB_ID}"
   db_error_comm "${MG_URL} could not be processed by cd-hit-dup"
   exit 2;
@@ -270,16 +284,16 @@ if [[ "$NUM_READS" -ne "$NUM_UNIQUE" ]]; then
 fi
 
 ###########################################################################################################
-# 8 - Calculate sequence statistics
+# 9 - Calculate sequence statistics
 ###########################################################################################################
 
 printf "Calculating sequence statistics..."
 
 # infoseq
-infoseq "${RAW_FASTA}" -only -pgc -length -noheading -auto > "${INFOSEQ_TMPFILE}"
+infoseq "${PROCESS_FASTA}" -only -pgc -length -noheading -auto > "${INFOSEQ_TMPFILE}"
 
 if [[ "$?" -ne "0" ]]; then  
-  email_comm "infoseq ${RAW_FASTA} -only -pgc -length -noheading -auto > ${INFOSEQ_TMPFILE}
+  email_comm "infoseq ${PROCESS_FASTA} -only -pgc -length -noheading -auto > ${INFOSEQ_TMPFILE}
 exited with RC $? in job ${JOB_ID} Files are at: ${FAILED_JOBS_DIR}/job-${JOB_ID}"
   db_error_comm "Cannot calculate sequence statistics. Please contact adminitrator."
   exit 2; 
@@ -307,58 +321,58 @@ printf "Number of bases: %d\nGC content: %f\nGC variance: %f\n" "${NUM_BASES}" "
 # define environment for sub jobs
 ###########################################################################################################
 
-echo frag_gene_scan=$frag_gene_scan >> 00-environment
-echo sina=$sina >> 00-environment
-echo sina_arb_pt_server=$sina_arb_pt_server >> 00-environment
-echo sina_version=$sina_version >> 00-environment
-echo sina_seed=$sina_seed >> 00-environment
-echo sina_seed_version=$sina_seed_version >> 00-environment
-echo sina_ref=$sina_ref >> 00-environment
-echo sina_ref_version=$sina_ref_version >> 00-environment
-echo SINA_SOCKET=$SINA_SOCKET >> 00-environment
+echo frag_gene_scan=$frag_gene_scan > 01-environment
+echo sina=$sina >> 01-environment
+echo sina_arb_pt_server=$sina_arb_pt_server >> 01-environment
+echo sina_version=$sina_version >> 01-environment
+echo sina_seed=$sina_seed >> 01-environment
+echo sina_seed_version=$sina_seed_version >> 01-environment
+echo sina_ref=$sina_ref >> 01-environment
+echo sina_ref_version=$sina_ref_version >> 01-environment
+echo SINA_SOCKET=$SINA_SOCKET >> 01-environment
 
-echo uproc=$uproc >> 00-environment
-echo uproc_pfam=$uproc_pfam >> 00-environment
-echo uproc_model=$uproc_model >> 00-environment
+echo uproc=$uproc >> 01-environment
+echo uproc_pfam=$uproc_pfam >> 01-environment
+echo uproc_model=$uproc_model >> 01-environment
 
-echo r_interpreter=$r_interpreter >> 00-environment
+echo r_interpreter=$r_interpreter >> 01-environment
 
-echo target_db_host=$target_db_host >> 00-environment
-echo target_db_port=$target_db_port >> 00-environment
-echo target_db_user=$target_db_user >> 00-environment
-echo target_db_name=$target_db_name >> 00-environment
-echo mt_admin_mail=$mt_admin_mail >> 00-environment
+echo target_db_host=$target_db_host >> 01-environment
+echo target_db_port=$target_db_port >> 01-environment
+echo target_db_user=$target_db_user >> 01-environment
+echo target_db_name=$target_db_name >> 01-environment
+echo mt_admin_mail=$mt_admin_mail >> 01-environment
 
-echo TFFILE=$TFFILE >> 00-environment
-echo PFAM_ACCESSIONS=$PFAM_ACCESSIONS >> 00-environment
-echo SLV_FILE=$SLV_FILE >> 00-environment
+echo TFFILE=$TFFILE >> 01-environment
+echo PFAM_ACCESSIONS=$PFAM_ACCESSIONS >> 01-environment
+echo SLV_FILE=$SLV_FILE >> 01-environment
 
-echo JOB_ID=$JOB_ID >> 00-environment
-echo SAMPLE_LABEL=$SAMPLE_LABEL >> 00-environment
-echo MG_ID=$MG_ID >> 00-environment
-echo NSLOTS=$NSLOTS >> 00-environment
+echo JOB_ID=$JOB_ID >> 01-environment
+echo SAMPLE_LABEL=$SAMPLE_LABEL >> 01-environment
+echo MG_ID=$MG_ID >> 01-environment
+echo NSLOTS=$NSLOTS >> 01-environment
 
-echo RAW_FASTA=$RAW_FASTA >> 00-environment
-echo GC=$GC >> 00-environment
-echo VARGC=$VARGC >> 00-environment
-echo NUM_BASES=$NUM_BASES >> 00-environment
-echo NUM_READS=$NUM_READS >> 00-environment
+echo PROCESS_FASTA=$PROCESS_FASTA >> 01-environment
+echo GC=$GC >> 01-environment
+echo VARGC=$VARGC >> 01-environment
+echo NUM_BASES=$NUM_BASES >> 01-environment
+echo NUM_READS=$NUM_READS >> 01-environment
 
-echo SINA_LOG_DIR=$SINA_LOG_DIR >> 00-environment
-echo ARBHOME=$ARBHOME >> 00-environment
-echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH >> 00-environment
-echo temp_dir=$temp_dir >> 00-environment
-echo THIS_JOB_TMP_DIR=$THIS_JOB_TMP_DIR >> 00-environment
-echo FAILED_JOBS_DIR=$FAILED_JOBS_DIR >> 00-environment
-echo RUNNING_JOBS_DIR=$RUNNING_JOBS_DIR >> 00-environment
-echo FINISHED_JOBS_DIR=$FINISHED_JOBS_DIR >> 00-environment
+echo SINA_LOG_DIR=$SINA_LOG_DIR >> 01-environment
+echo ARBHOME=$ARBHOME >> 01-environment
+echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH >> 01-environment
+echo temp_dir=$temp_dir >> 01-environment
+echo THIS_JOB_TMP_DIR=$THIS_JOB_TMP_DIR >> 01-environment
+echo FAILED_JOBS_DIR=$FAILED_JOBS_DIR >> 01-environment
+echo RUNNING_JOBS_DIR=$RUNNING_JOBS_DIR >> 01-environment
+echo FINISHED_JOBS_DIR=$FINISHED_JOBS_DIR >> 01-environment
 
 ###########################################################################################################
 # 1 - run fgs
 ###########################################################################################################
 
 #Split original
-awk -vn="${NSEQ}" 'BEGIN {n_seq=0;partid=1;} /^>/ {if(n_seq%n==0){file=sprintf("05-part-%d.fasta",partid);partid++;} print >> file; n_seq++; next;} { print >> file; }' < "${RAW_FASTA}"
+awk -vn="${NSEQ}" 'BEGIN {n_seq=0;partid=1;} /^>/ {if(n_seq%n==0){file=sprintf("05-part-%d.fasta",partid);partid++;} print >> file; n_seq++; next;} { print >> file; }' < "${PROCESS_FASTA}"
 NFILES=$(ls -1 05-part*.fasta | wc -l)
 
 "${fgs_runner}" "${NSLOTS}" "${NFILES}" "${FGS_JOBARRAYID}"
@@ -379,7 +393,7 @@ fi
 
 #MEM=$(free -m | grep Mem | awk '{printf "%d",$2/3}')
 MEM=4000
-$sortmerna --reads "${RAW_FASTA}" -a ${NSLOTS} --ref \
+$sortmerna --reads "${PROCESS_FASTA}" -a ${NSLOTS} --ref \
 ${DB}/rRNA_databases/silva-bac-16s-id90.fasta,\
 ${DB}/index/silva-bac-16s-db:${DB}/rRNA_databases/silva-arc-16s-id95.fasta,\
 ${DB}/index/silva-arc-16s-db:${DB}/rRNA_databases/silva-euk-18s-id95.fasta,\
@@ -387,7 +401,7 @@ ${DB}/index/silva-euk-18s-db --blast 1 --fastx --aligned "${SORTMERNA_OUT}" -v -
 
 
 if [[ "${ERROR_SORTMERNA}" -ne "0" ]]; then
-  email_comm "${sortmerna} --reads ${RAW_FASTA} -a ${NSLOTS} --ref ${DB}/rRNA_databases/silva-bac-16s-id90.fasta ...
+  email_comm "${sortmerna} --reads ${PROCESS_FASTA} -a ${NSLOTS} --ref ${DB}/rRNA_databases/silva-bac-16s-id90.fasta ...
 exited with RC ${ERROR_SORTMERNA} in job ${JOB_ID}."
   db_error_comm "sortmerna failed. Please contact adminitrator"
   exit 2
